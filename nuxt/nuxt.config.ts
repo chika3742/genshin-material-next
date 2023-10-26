dbfc4d140205b90fc435d9156e8589fe110c8f59
chika3742 <kazu.chika.shima@gmail.com>
2023-10-26 16:42:36 +0900

    feat: @chika3742/mhy-material-components を追加

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: {enabled: true},
+  modules: [
+    "@chika3742/mhy-material-components",
+  ],
 })
