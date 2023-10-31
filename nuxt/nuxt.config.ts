71e44b4d3d0ea41a145e41fd126af8063e11d1d6
chika3742 <kazu.chika.shima@gmail.com>
2023-10-31 16:12:43 +0900

    feat: 基本的なNuxtの設定

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,6 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {enabled: true},
+  app: {
+    pageTransition: {
+      name: "scroll-y-reverse-transition",
+      leaveActiveClass: "position-absolute d-none",
+      duration: 200,
+    },
+    head: {
+      link: [
+        {
+          rel: "shortcut icon",
+          href: "/favicon.webp",
+        },
+        {
+          rel: "manifest",
+          href: "/manifest.webmanifest",
+        },
+      ],
+      meta: [
+        {
+          name: "viewport",
+          content: "viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        },
+      ],
+    },
+  },
+  experimental: {
+    payloadExtraction: false,
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  nitro: {
+    preset: "cloudflare-pages-static",
+  },
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
+  typescript: {
+    tsConfig: {
+      exclude: ["functions"],
+    },
+  },
+
   modules: [
     "@chika3742/mhy-material-components",
   ],
