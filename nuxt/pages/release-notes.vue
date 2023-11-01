2344e45adae22292f650fa09838e01e7a1a7f970
chika3742 <kazu.chika.shima@gmail.com>
2023-11-01 20:13:34 +0900

    feat: 更新履歴ページ

---
+++ nuxt/pages/release-notes.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import releaseNotes from "~/assets/data/release-notes.yaml"
+
+definePageMeta({
+  title: "releaseNotes",
+})
+</script>
+
+<template>
+  <PageReleaseNotes :release-notes="releaseNotes" />
+</template>
+
+<style scoped lang="sass">
+
+</style>
